baeea5ae0ac1168dae7f6b029917a4fdb84f84de
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2023-07-13 16:49:13 +0300

    last commit before starting the backend

--- client/MusicPlayer/src/App.jsx
+++ client/MusicPlayer/src/App.jsx
@@ -3,30 +3,38 @@ import './App.css';
 import songs from './songs';
 
 function App() {
-  const [source, setSource] = useState('');
-
+  const [source, setSource] = useState('0');
+  const [thumb, setThumb] = useState(0);
+  const [tittle, setTittle] = useState(0);
   return (
     <>
       <div className='top-nav'></div>
       <div className='middle-nav'>
         {songs.map((song, index) => (
-          <Card key={index} song={song} setSource={setSource} />
+          <Card key={index} song={song} setSource={setSource} setThumb={setThumb} setTittle={setTittle} />
         ))}
       </div>
       <div className='bottom-nav'>
-        <audio src={source} controls />
+        {thumb != 0 ? <div className='show-song'>
+          <img src={thumb}></img>
+          <p>{tittle}</p>
+          </div>: null}
+        {source!=0 ? <audio src={source} controls /> : null}
       </div>
     </>
   );
 }
 
-function Card({ song, setSource }) {
+function Card({ song, setSource, setThumb, setTittle }) {
   function handleClick() {
     setSource(song.song);
+    setThumb(song.image);
+    setTittle(song.name);
   }
 
   return (
     <div className='card' onClick={handleClick}>
+     <img src={song.image}></img>
       <p>{song.name}</p>
     </div>
   );
--- client/MusicPlayer/src/songs.jsx
+++ client/MusicPlayer/src/songs.jsx
@@ -7,45 +7,55 @@ import ShakeItoff from './pesni/Shake it Of.mp3';
 import StariRani from './pesni/Stari rani.mp3';
 import TeAMO from './pesni/Te Amo.mp3';
 import colapse from './pesni/Till i colapse.mp3';
+import image from './image.jpg'
 
 
 
 //const songs = [RapGod, Leva10, Jiva, NOtAfraid, OldTownRoad, ShakeItoff, StariRani, TeAMO, colapse]
 const songs = [{
     name: "RapGod",
-    song: RapGod
+    song: RapGod,
+    image: image
 },
 {
     name: "10 leva",
-    song: Leva10
+    song: Leva10,
+    image: image
 },
 {
     name: "jiva rana",
-    song: Jiva
+    song: Jiva,
+    image: image
 },
 {
     name: "Not Afraid",
-    song: NOtAfraid
+    song: NOtAfraid,
+    image: image
 },
 {
     name: "Old Town Road",
-    song: OldTownRoad
+    song: OldTownRoad,
+    image: image
 },
 {
     name: "Shake it Of",
-    song: ShakeItoff
+    song: ShakeItoff,
+    image: image
 },
 {
     name: "Stari rani",
-    song: StariRani
+    song: StariRani,
+    image: image
 },
 {
     name: "Te Amo",
-    song: TeAMO
+    song: TeAMO,
+    image: image
 },
 {
     name: "Till I colapse",
-    song: colapse
+    song: colapse,
+    image: image
 }
 ]
 const song = RapGod;
