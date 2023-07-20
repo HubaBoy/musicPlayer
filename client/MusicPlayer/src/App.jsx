077c4771179bb1e55db3cfcbebe6ccf1da1cced6
HubaBoy <velizar.tsiklovski@gmail.com>
2023-07-20 03:08:27 +0300

    fixing the thumbnail fetching

--- client/MusicPlayer/src/App.jsx
+++ client/MusicPlayer/src/App.jsx
@@ -66,19 +66,40 @@ function Card({ song, setSource, setThumb, setTitle }) {
       console.log(data);
     })
     .catch(error => console.log(error));
-    if(song.thumbnail == null)
+    if(thumbnail == null)
     {
       setThumb(image)
     }
     else{
-      setThumb(song.thumbnail);
+      setThumb(thumbnail);
     }
     setTitle(song.title);
   }
 
+  const [thumbnail, setThumbnail] = useState(null);
+  useEffect(() => {
+  fetch(`http://localhost:3000/thumbnail/${song.id}`)
+  .then( results => {
+    if(!results.ok)
+    {throw new Error('Error retrieving the thumbnail')}
+    return results.blob()
+  }
+  )
+  .then(
+    data => {
+      const dataURL = URL.createObjectURL(data)
+      setThumbnail(dataURL);
+    }
+  )
+  .catch(error =>
+    {
+      console.error(error);
+    })
+}, [])
+
   return (
     <div className='card' onClick={handleClick}>
-      <img src={song.thumbnail !== null ?song.thumbnail : image} alt='thumbnail' />
+      <img src={thumbnail !== null ? thumbnail : image} alt='thumbnail' />
       <p>{song.title}</p>
     </div>
   );
