38f1d943caf64b8c6a9fb756f2263969f4fb6edf
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2023-07-25 20:26:51 +0300

    .

--- client/MusicPlayer/src/App.jsx
+++ client/MusicPlayer/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import image from './image.jpg'
 import styled from 'styled-components'
 import {createBrowserRouter,
   createRoutesFromElements,
