9e6e54ea5aa5dd8152a6ff698f0f7a172655a542
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2023-08-28 16:47:41 +0300

    version 1.00 electron app

---
+++ client/my-electron-app/main.js
@@ -0,0 +1,30 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // This allows you to use Node.js modules in your renderer process.
+    },
+  });
+
+  // Load your React app (assuming it's built into a build folder)
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+// Quit the app when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
