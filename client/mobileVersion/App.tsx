81d0d36953e7402b1ff6cc7738198df5c5216e1b
HubaBoy <velizar.tsiklovski@gmail.com>
2023-09-12 21:36:32 +0300

    middleNav bar added to mobile app

--- client/mobileVersion/App.tsx
+++ client/mobileVersion/App.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 const YourApp = () => {
   return (
-   <>
-    <ScrollView contentContainerStyle={styles.topNav}>
-    </ScrollView>
-    <ScrollView contentContainerStyle={styles.bottomNav}>
-      
-    </ScrollView>
-   </>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.topNav} style={styles.scrollView}>
+        {/* TopNav Content */}
+      </ScrollView>
+      <ScrollView contentContainerStyle={styles.middleNav} style={styles.scrollView}>
+      </ScrollView>
+      <ScrollView contentContainerStyle={styles.bottomNav} style={styles.scrollView}>
+        {/* BottomNav Content */}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    margin: 0,
+  },
   topNav: {
-    height: 100, 
+    position: 'absolute',
+    height: 100,
     width: '100%',
     backgroundColor: 'black',
     display: 'flex',
@@ -26,11 +36,20 @@ const styles = StyleSheet.create({
   bottomNav: {
     position: 'absolute',
     width: '100%',
+    marginBottom: 0,
     bottom: 0,
     left: 0,
     right: 0,
     height: 100,
     backgroundColor: '#5ADBFF',
+  },
+  middleNav: {
+    backgroundColor: 'orange',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingLeft: '5%',
+    paddingBottom: '11%',
+    minHeight: '69%',
   },
 });
 
